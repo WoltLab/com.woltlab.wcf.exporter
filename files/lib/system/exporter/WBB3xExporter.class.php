2f118389c9d7d0e6574793392e0ca7ca487a9072
Marcel Werk <burntime@woltlab.com>
2013-06-29 19:08:23 +0200

    Initial version (added WBB3 support)

---
+++ files/lib/system/exporter/WBB3xExporter.class.php
@@ -0,0 +1,245 @@
+<?php
+namespace wcf\system\exporter;
+use wcf\system\database\DatabaseException;
+use wcf\system\exception\SystemException;
+use wcf\system\importer\ImportHandler;
+
+/**
+ * Exporter for Burning Board 3.x
+ *
+ * @author	Marcel Werk
+ * @copyright	2001-2012 WoltLab GmbH
+ * @license	WoltLab Burning Board License <http://www.woltlab.com/products/burning_board/license.php>
+ * @package	com.woltlab.wcf.exporter
+ * @subpackage	system.exporter
+ * @category	Community Framework (commercial)
+ */
+class WBB3xExporter extends AbstractExporter {
+	/**
+	 * wcf installation number
+	 * @var integer
+	 */
+	protected $dbNo = 1;
+	
+	/**
+	 * wbb installation number
+	 * @var integer
+	 */
+	protected $instanceNo = 1;
+	
+	/**
+	 * selected import data
+	 * @var array
+	 */
+	protected $selectedData = array();
+	
+	/**
+	 * @see wcf\system\exporter\AbstractExporter::$methods
+	 */
+	protected $methods = array(
+		'com.woltlab.wcf.user' => 'Users',
+		'com.woltlab.wcf.user.group' => 'UserGroups'
+	);
+	
+	/**
+	 * @see wcf\system\exporter\IExporter::init()
+	 */
+	public function init() {
+		parent::init();
+		
+		if (!preg_match('/^wbb(\d)_(\d)_$/', $this->databasePrefix, $match)) {
+			$this->dbNo = $match[1];
+			$this->instanceNo = $match[2];
+		}
+	}
+	
+	/**
+	 * @see wcf\system\exporter\IExporter::getSupportedData()
+	 */
+	public function getSupportedData() {
+		return array(
+			'com.woltlab.wcf.user' => array(
+				'com.woltlab.wcf.user.group',
+				'com.woltlab.wcf.user.avatar',
+				'com.woltlab.wcf.user.option',
+				'com.woltlab.wcf.user.comment',
+				'com.woltlab.wcf.user.follower'
+			),
+			'com.woltlab.wbb.board' => array(
+				'com.woltlab.wbb.moderator',
+				'com.woltlab.wbb.acl',
+				'com.woltlab.wbb.attachment',
+				'com.woltlab.wbb.poll',
+				'com.woltlab.wbb.watchedThread',
+			),
+			'com.woltlab.wcf.conversation' => array(
+				'com.woltlab.wcf.conversation.attachment',
+			),
+			'com.woltlab.wcf.smiley' => array()
+		);
+	}
+	
+	/**
+	 * @see wcf\system\exporter\IExporter::validateDatabaseAccess()
+	 */
+	public function validateDatabaseAccess() {
+		if (!parent::validateDatabaseAccess()) return false;
+		
+		if (!preg_match('/^wbb\d_\d_$/', $this->databasePrefix)) return false;
+		
+		try {
+			$sql = "SELECT COUNT(*) FROM ".$this->databasePrefix."post";
+			$statement = $this->database->prepareStatement($sql);
+			$statement->execute();
+		}
+		catch (DatabaseException $e) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @see wcf\system\exporter\IExporter::validateFileAccess()
+	 */
+	public function validateFileAccess() {
+		if (in_array('com.woltlab.wcf.user.avatar', $this->selectedData) || in_array('com.woltlab.wbb.attachment', $this->selectedData) || in_array('com.woltlab.wcf.conversation.attachment', $this->selectedData)) {
+			if (empty($this->fileSystemPath) || !@file_exists($this->fileSystemPath . 'lib/core.functions.php')) return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @see wcf\system\exporter\IExporter::validateSelectedData()
+	 */
+	public function validateSelectedData(array $selectedData) {
+		$this->selectedData = $selectedData;
+		
+		return true;
+	}
+	
+	/**
+	 * @see wcf\system\exporter\IExporter::getQueue()
+	 */
+	public function getQueue() {
+		$queue = array();
+		
+		// user
+		if (in_array('com.woltlab.wcf.user', $this->selectedData)) {
+			if (in_array('com.woltlab.wcf.user.group', $this->selectedData)) $queue[] = 'com.woltlab.wcf.user.group';
+			if (in_array('com.woltlab.wcf.user.option', $this->selectedData)) $queue[] = 'com.woltlab.wcf.user.option';
+			$queue[] = 'com.woltlab.wcf.user';
+			if (in_array('com.woltlab.wcf.user.avatar', $this->selectedData)) $queue[] = 'com.woltlab.wcf.user.avatar';
+			
+			if (in_array('com.woltlab.wcf.user.comment', $this->selectedData)) {
+				$queue[] = 'com.woltlab.wcf.user.comment';
+				$queue[] = 'com.woltlab.wcf.user.comment.response';
+			}
+			
+			if (in_array('com.woltlab.wcf.user.follower', $this->selectedData)) $queue[] = 'com.woltlab.wcf.user.follower';
+			
+			// conversation
+			if (in_array('com.woltlab.wcf.conversation', $this->selectedData)) {
+				$queue[] = 'com.woltlab.wcf.conversation';
+				$queue[] = 'com.woltlab.wcf.conversation.message';
+				$queue[] = 'com.woltlab.wcf.conversation.user';
+					
+				if (in_array('com.woltlab.wcf.conversation.attachment', $this->selectedData)) $queue[] = 'com.woltlab.wcf.conversation.attachment';
+			}
+		}
+		
+		// board
+		if (in_array('com.woltlab.wcf.board', $this->selectedData)) {
+			$queue[] = 'com.woltlab.wcf.board';
+			$queue[] = 'com.woltlab.wcf.thread';
+			$queue[] = 'com.woltlab.wcf.post';
+			
+			if (in_array('com.woltlab.wbb.moderator', $this->selectedData)) $queue[] = 'com.woltlab.wbb.moderator';
+			if (in_array('com.woltlab.wbb.acl', $this->selectedData)) $queue[] = 'com.woltlab.wbb.acl';
+			if (in_array('com.woltlab.wbb.attachment', $this->selectedData)) $queue[] = 'com.woltlab.wbb.attachment';
+			if (in_array('com.woltlab.wbb.watchedThread', $this->selectedData)) $queue[] = 'com.woltlab.wbb.watchedThread';
+			if (in_array('com.woltlab.wcf.poll', $this->selectedData)) {
+				$queue[] = 'com.woltlab.wcf.poll';
+				$queue[] = 'com.woltlab.wcf.poll.option';
+				$queue[] = 'com.woltlab.wcf.poll.option.vote';
+			}
+		}
+		
+		// smiley
+		if (in_array('com.woltlab.wcf.smiley', $this->selectedData)) $queue[] = 'com.woltlab.wcf.smiley';
+		
+		return $queue;
+	}
+	
+	/**
+	 * @see wcf\system\exporter\IExporter::getDefaultDatabasePrefix()
+	 */
+	public function getDefaultDatabasePrefix() {
+		return 'wbb1_1_';
+	}
+	
+	/**
+	 * Counts user groups.
+	 */
+	public function countUserGroups() {
+		$sql = "SELECT	COUNT(*) AS count
+			FROM	wcf".$this->dbNo."_group
+			WHERE	groupType > ?";
+		$statement = $this->database->prepareStatement($sql);
+		$statement->execute(array(3));
+		$row = $statement->fetchArray();
+		return $row['count'];
+	}
+	
+	/**
+	 * Exports user groups.
+	 */
+	public function exportUserGroups($offset, $limit) {
+		$sql = "SELECT		*
+			FROM		wcf".$this->dbNo."_group
+			WHERE		groupType > ?
+			ORDER BY	groupID";
+		$statement = $this->database->prepareStatement($sql, $limit, $offset);
+		$statement->execute(array(3));
+		while ($row = $statement->fetchArray()) {
+			ImportHandler::getInstance()->getImporter('com.woltlab.wcf.user.group')->import($row['groupID'], array(
+				'groupName' => $row['groupName'],
+				'groupType' => $row['groupType'],
+				'userOnlineMarking' => $row['userOnlineMarking'],
+				'showOnTeamPage' => $row['showOnTeamPage']
+			));
+		}
+	}
+
+	/**
+	 * Counts users.
+	 */
+	public function countUsers() {
+		$sql = "SELECT	COUNT(*) AS count
+			FROM	wcf".$this->dbNo."_user";
+		$statement = $this->database->prepareStatement($sql);
+		$statement->execute();
+		$row = $statement->fetchArray();
+		return $row['count'];
+	}
+	
+	/**
+	 * Exports users.
+	 */
+	public function exportUsers($offset, $limit) {
+		$sql = "SELECT		*
+			FROM		wcf".$this->dbNo."_user
+			ORDER BY	userID";
+		$statement = $this->database->prepareStatement($sql, $limit, $offset);
+		$statement->execute();
+		while ($row = $statement->fetchArray()) {
+			ImportHandler::getInstance()->getImporter('com.woltlab.wcf.user')->import($row['userID'], array(
+				'username' => $row['username'],
+				'password' => 'wcf1:'.$row['salt'].':'.$row['password'],
+				'email' => $row['email'],
+				'registrationDate' => $row['registrationDate']
+			));
+		}
+	}
+}
