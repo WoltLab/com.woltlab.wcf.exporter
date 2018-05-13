416a95ccc365fe8f7065e11e2fd6cea20104897e
Tim Düsterhus <tim@bastelstu.be>
2018-05-13 14:21:12 +0200

    vB: Fix attachment export in vBulletin 3

--- files/lib/system/exporter/VB3or4xExporter.class.php
+++ files/lib/system/exporter/VB3or4xExporter.class.php
@@ -2002,9 +2002,9 @@ class VB3or4xExporter extends AbstractExporter {
 			// vb 3
 			if ($contentType != 'Post') return 0;
 			
-			$sql = "SELECT		*
-				FROM		".$this->databasePrefix."attachment
-				ORDER BY	attachmentid";
+			$sql = "SELECT		attachment.*, attachment.postid AS contentid
+				FROM		".$this->databasePrefix."attachment attachment
+				ORDER BY	attachment.attachmentid";
 			$statement = $this->database->prepareStatement($sql, $limit, $offset);
 			$statement->execute();
 		}
